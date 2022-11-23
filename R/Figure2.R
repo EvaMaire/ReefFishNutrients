553ca457b95f6b1957c571a6ded7ee87ff7ea71a
EvaMaire <emg.maire@gmail.com>
2022-11-23 11:31:51 +0000

    update libraries and paths

--- R/Figure2.R
+++ R/Figure2.R
@@ -1,110 +1,85 @@
-setwd("/Volumes/EM2T/Lancaster/Nutrition/SERF/nutrient_value/dag-based")
-
 ######################
 ## LIBRARY PACKAGES ##
 ######################
 
-library(data.table)
-library(mapproj)
-library(maptools)
-library(plot3D)
-library(pals)
-#library(MuMIn)
 library(bayestestR)
-library(insight)
 library(brms)
 library(bayesplot)
-library(performance)
 library(parallel)
 library(rstan)
-library(readr)
 library(tidyverse)
 library(dplyr)
+library(here)
 library(patchwork)
 library(rethinking)
 
+here()
 
 ##############################
 ## IMPORT ALL FITTED MODELS ##
 ##############################
 
-full_model <- readRDS("output/full_model.rds")
-full_post <- as.data.frame(as.matrix(full_model)) %>%
-  select('b_biomass':'b_MPAUnfishedHigh')
-full_estimates <- data.frame(median=apply(full_post, 2, median))
-full_estimates$abs_effect <- abs(full_estimates$median)
-full_estimates <- full_estimates %>%
-  arrange(desc(abs_effect))
-full_post <- full_post[,order(match(colnames(full_post), rownames(full_estimates)))]
-
-biomass_model <- readRDS("output/biomass_model.rds")
+biomass_model <- readRDS("outputs/biomass_model.rds")
 biomass_post <- as.data.frame(as.matrix(biomass_model)) %>%
   select('b_biomass')
 
-#fishcompo_model <- readRDS("output/fishcompo_model.rds")
-#fishcompo_post <- as.data.frame(as.matrix(fishcompo_model)) %>%
-#select('b_fishcompo')
-PC1_model <- readRDS("output/PC1_model.rds")
+PC1_model <- readRDS("outputs/PC1_model.rds")
 PC1_post <- as.data.frame(as.matrix(PC1_model)) %>%
   select('b_PC1')
 
-PC2_model <- readRDS("output/PC2_model.rds")
+PC2_model <- readRDS("outputs/PC2_model.rds")
 PC2_post <- as.data.frame(as.matrix(PC2_model)) %>%
   select('b_PC2')
 
-fishdiversity_model <- readRDS("output/fishdiversity_model.rds")
+fishdiversity_model <- readRDS("outputs/fishdiversity_model.rds")
 fishdiversity_post <- as.data.frame(as.matrix(fishdiversity_model)) %>%
   select('b_fishdiversity')
 
-gravity_model <- readRDS("output/gravity_model.rds")
+gravity_model <- readRDS("outputs/gravity_model.rds")
 gravity_post <- as.data.frame(as.matrix(gravity_model)) %>%
   select('b_gravity')
 
-depth_model <- readRDS("output/depth_model.rds")
+depth_model <- readRDS("outputs/depth_model.rds")
 depth_post <- as.data.frame(as.matrix(depth_model)) %>%
   select('b_depth>10m','b_depth0M4m')
 
-NPP_model <- readRDS("output/NPP_model.rds")
+NPP_model <- readRDS("outputs/NPP_model.rds")
 NPP_post <- as.data.frame(as.matrix(NPP_model)) %>%
   select('b_NPP')
 
-meantemp_model <- readRDS("output/meantemp_model.rds")
+meantemp_model <- readRDS("outputs/meantemp_model.rds")
 meantemp_post <- as.data.frame(as.matrix(meantemp_model)) %>%
   select('b_meantemp')
 
-rangetemp_model <- readRDS("output/rangetemp_model.rds")
+rangetemp_model <- readRDS("outputs/rangetemp_model.rds")
 rangetemp_post <- as.data.frame(as.matrix(rangetemp_model)) %>%
   select('b_rangetemp')
 
-HDI_model <- readRDS("output/HDI_model.rds")
+HDI_model <- readRDS("outputs/HDI_model.rds")
 HDI_post <- as.data.frame(as.matrix(HDI_model)) %>%
   select('b_HDI')
 
-MPA_model <- readRDS("output/MPA_model.rds")
+MPA_model <- readRDS("outputs/MPA_model.rds")
 MPA_post <- as.data.frame(as.matrix(MPA_model)) %>%
   select('b_MPARestricted','b_MPAUnfishedHigh')
 
-dhw_model <- readRDS("output/dhw_model.rds")
+dhw_model <- readRDS("outputs/dhw_model.rds")
 dhw_post <- as.data.frame(as.matrix(dhw_model)) %>%
   select('b_maxdhw')
 
-bwsize_model <- readRDS("output/bwsize_model.rds")
+bwsize_model <- readRDS("outputs/bwsize_model.rds")
 bwsize_post <- as.data.frame(as.matrix(bwsize_model)) %>%
   select('b_bw_size')
 
-voice_model <- readRDS("output/voice_model.rds")
+voice_model <- readRDS("outputs/voice_model.rds")
 voice_post <- as.data.frame(as.matrix(voice_model)) %>%
   select('b_voice')
 
-#coralcover_model <- readRDS("output/coralcover_model.rds")
-#coralcover_post <- as.data.frame(as.matrix(coralcover_model)) %>%
-#select('bsp_nutrientscore_miCoralCover')
-
-geomorphology_model <- readRDS("output/geomorphology_model.rds")
+geomorphology_model <- readRDS("outputs/geomorphology_model.rds")
 geomorphology_post <- as.data.frame(as.matrix(geomorphology_model)) %>%
   select('b_geomorphologyCrest','b_geomorphologyFlat','b_geomorphologyLagoon_Backreef')
 
-wave_energy_model <- readRDS("output/wave_energy_model.rds")
+wave_energy_model <- readRDS("outputs/wave_energy_model.rds")
 wave_energy_post <- as.data.frame(as.matrix(wave_energy_model)) %>%
   select('b_wave_energy')
 
@@ -112,20 +87,18 @@ wave_energy_post <- as.data.frame(as.matrix(wave_energy_model)) %>%
 ## RECOMBINE DAG MODELS AND RENAME VARIABLES ##
 ###############################################
 
-dag_output <- data.frame(biomass_post,PC1_post,PC2_post,#fishcompo_post,
+dag_output <- data.frame(biomass_post,PC1_post,PC2_post,
                          fishdiversity_post,gravity_post,
                          depth_post,NPP_post,meantemp_post,rangetemp_post,
-                         HDI_post,MPA_post,dhw_post,bwsize_post,voice_post,#coralcover_post,
+                         HDI_post,MPA_post,dhw_post,bwsize_post,voice_post,
                          geomorphology_post,wave_energy_post)
 
-#names(full_post) <- gsub("b_", "", names(full_post))
 names(dag_output) <- gsub("b_", "", names(dag_output))
 
 dag_output <- dag_output %>%
   rename('Standing biomass'=biomass ,
          "PC1: herbivore/detritivore"=PC1,
          "PC2: piscivore (+) / invertivore (-)"=PC2,
-         #'herb/detri'=fishcompo ,
          'Species richness'=fishdiversity  ,
          'Human gravity'=gravity ,
          'Deep reef: >10m'=depth.10m ,
@@ -138,7 +111,6 @@ dag_output <- dag_output %>%
          'DHW'=maxdhw    ,
          'Mean fish size'=bw_size,
          'Voice and accountability'=voice ,
-         #'coral cover'= bsp_nutrientscore_miCoralCover ,
          'Reef flat'=geomorphologyFlat ,
          'Lagoon'=geomorphologyLagoon_Backreef ,
          'Reef crest'=geomorphologyCrest,
@@ -151,29 +123,17 @@ dag_estimates <- dag_estimates %>%
 
 dag_output <- dag_output[,order(match(colnames(dag_output), rownames(dag_estimates)))]
 
-p <- mcmc_intervals(dag_output) +
-  #ggtitle("DAG-Based Models - Nutrient score calcium/iron/zinc") +
-  xlim(range(dag_output))
-
-tiff("/Volumes/EM2T/Lancaster/Nutrition/SERF/nutrient_value/dag-based/Figure2.tiff", width=2000, height=2600, compression="lzw", res=300) 
-p
-graphics.off()
-
 #Other option
 post <- dag_output
 
-test <- mcmc_intervals_data(dag_output, prob = 0.5,
+post_interv <- mcmc_intervals_data(dag_output, prob = 0.5,
   prob_outer = 0.9,
   point_est = c("median") )
 
-##p_median <- apply(post,2,median)
-#p_HPDI_1_95 <- hdi(post, prob = c(.5, .9)) #apply(post,2,HPDI,prob=0.9)
-#p_HPDI_1_50 <- apply(post,2,HPDI,prob=0.5)
-HPDI_1 <- data.frame(test)
+HPDI_1 <- data.frame(post_interv)
 HPDI_1 <- HPDI_1 %>% select(!c(outer_width:point_est))
 names(HPDI_1)<-c("Var","ll0.05","ll0.25","median","ul0.75","ul0.95")
 rownames(HPDI_1) <- HPDI_1$Var
-#HPDI_1$Var <- rownames(HPDI_1)
 
 HPDI_1$abs <- abs(HPDI_1$median)
 HPDI_1$zero50 <- ifelse(HPDI_1$ll0.25<0 & HPDI_1$ul0.75 >0,1,0)
@@ -188,7 +148,6 @@ HPDI_1$zero[which(HPDI_1$zero50 == 1 & HPDI_1$zero95 == 1)] <- 1
 HPDI_1
 
 #create matrix for reference levels (Slope, Fished Areas and average depth)
-
 ref <- HPDI_1[c(1:3),]
 rownames(ref) = c("Mid-depth reef (4-10m)","Slope","Fished areas")
 ref$Var <- c("Mid-depth reef (4-10m)","Slope","Fished areas")
@@ -196,6 +155,7 @@ ref[,c(2:13)] <- 0
 
 HPDI <- rbind(HPDI_1,ref)
 
+#reorder drivers 
 roworder <- rev(c("PC1: herbivore/detritivore","Mean fish size","Species richness","Standing biomass","PC2: piscivore (+) / invertivore (-)", #Species composition
                   "Human gravity","Voice and accountability","HDI", #Social
                   "Shallow reef: 0-4m","Deep reef: >10m","Mid-depth reef (4-10m)","Reef flat","Reef crest","Lagoon","Slope", #local env.
@@ -208,10 +168,10 @@ datFig$Var <- factor(datFig$Var, levels = datFig$Var)
 #Set up color and size vectors
 datFig$col <- rep("white",nrow(datFig)) 
 datFig$col[which(datFig$zero == 1)] <- "white"
-datFig$col[which(datFig$positive==1 & datFig$strong==1)] <- "#006666" #dark green ( strong positive)
+datFig$col[which(datFig$positive==1 & datFig$strong==1)] <- "#006666" #dark green (strong positive)
 datFig$col[which(datFig$positive==0 & datFig$strong==1)] <- "#862d59" #dark red (strong negative)
-datFig$col[which(datFig$positive == 1 & datFig$light == 1)] <- "#00666680" #light green
-datFig$col[which(datFig$positive == 0 & datFig$light == 1)] <- "#862d5980"
+datFig$col[which(datFig$positive == 1 & datFig$light == 1)] <- "#00666680" #increase transparency for small effect
+datFig$col[which(datFig$positive == 0 & datFig$light == 1)] <- "#862d5980" #increase transparency for small effect
 
 datFig$size <- rep(4,nrow(datFig))
 datFig$size[which(datFig$strong==1)] <- 6
@@ -232,10 +192,6 @@ white_themejpg <-theme(axis.ticks=element_line(colour="black"),
 
 
 effect <- ggplot(datFig ,aes(x=Var, y=median)) + 
-  #annotate("rect", ymin = min(datFig$ll0.05)+min(datFig$ll0.05)/6, ymax = max(datFig$ul0.95)+max(datFig$ll0.05)/6, xmin = 14.5, xmax = 16.5, alpha = .7, fill="#ffa31a10")+ #Management
-  #annotate("rect", ymin = min(datFig$ll0.05)+min(datFig$ll0.05)/6, ymax = max(datFig$ul0.95)+max(datFig$ll0.05)/6, xmin = 9.5, xmax = 14.5, alpha = .7,  fill="#002b8050")+ #Social Drivers
-  #annotate("rect", ymin = min(datFig$ll0.05)+min(datFig$ll0.05)/6, ymax = max(datFig$ul0.95)+max(datFig$ll0.05)/6, xmin = 2.5, xmax = 9.5, alpha = .7,   fill="#40bf8050")+ #Env
-  #annotate("rect", ymin = min(datFig$ll0.05)+min(datFig$ll0.05)/6, ymax = max(datFig$ul0.95)+max(datFig$ll0.05)/6, xmin = 0.5, xmax = 2.5, alpha = .7,   fill="#52527a50")+ #Method
   geom_vline(xintercept=3.5, lwd=0.5, lty=1)+
   geom_vline(xintercept=15.5, lwd=0.5, lty=1)+
   geom_vline(xintercept=18.5, lwd=0.5, lty=1)+
@@ -250,7 +206,7 @@ effect <- ggplot(datFig ,aes(x=Var, y=median)) +
 
 effect
 
-tiff("/Volumes/EM2T/Lancaster/Nutrition/SERF/nutrient_value/dag-based/Figure2V2.tiff", width=2500, height=2600, compression="lzw", res=300) 
+tiff("figures/Figure2.tiff", width=2500, height=2600, compression="lzw", res=300) 
 effect
 graphics.off()
 
